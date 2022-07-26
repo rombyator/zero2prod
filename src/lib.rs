4ba9e3f7d47101b644b03ad8395f235f9f9e3b14
Roman Korenev <rombyator@yandex.ru>
2022-07-26 18:51:55 +0200

    add subscribe route

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use actix_web::dev::Server;
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().service(health_check))
+    let server = HttpServer::new(|| App::new().service(health_check).service(subscribe))
         .listen(listener)?
         .run();
 
@@ -12,5 +12,16 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
 
 #[get("/health_check")]
 async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+#[post("/subscriptions")]
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
     HttpResponse::Ok()
 }
